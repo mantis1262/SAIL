7c4ceafa8fb1195b28fc8355b88e3e8e88fb6892
Mantis1262 <Mateuszkarolak3@o2.pl>
2019-11-13 14:53:50 +0100

    ulepszanie uczelni, naprawiono problem z kilkoma projektami vs

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Building : MonoBehaviour
 {
     [SerializeField] public List<GameObject> partBuildingList;
+    [SerializeField] public List<GameObject> awakeBuildingList;
+    [SerializeField] public Text helpBuy; 
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        foreach (GameObject game in awakeBuildingList)
+            game.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Buy()
     {
-        
+        if (partBuildingList.Count != 0)
+        {
+            Resoucres.grant--;
+            partBuildingList[0].SetActive(true);
+            awakeBuildingList.Add(partBuildingList[0]);
+            partBuildingList.RemoveAt(0);
+        }
     }
 }
+    
---
+++ Assets/Scripts/Resoucres.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resoucres : MonoBehaviour
+{
+
+    [SerializeField] public static int grant;
+    [SerializeField] private Text grantValue; 
+    void Start()
+    {
+        grant = 2; 
+    }
+
+    private void Update()
+    {
+        grantValue.text = grant.ToString();
+    }
+
+
+
+}
